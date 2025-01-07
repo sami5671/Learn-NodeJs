12fdfbf72f5128d958832f2dddd3a949ef05a879
sami5671 <samialam5671@gmail.com>
2025-01-07 15:01:47 +0600

    basic set up of the chat app in NODEJS

---
+++ Node Chat Application/app.js
@@ -0,0 +1,51 @@
+// external imports
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const loginRouter = require("./router/loginRouter");
+
+// internal imports
+const {
+  errorHandler,
+  notFoundHandler,
+} = require("./middlewares/common/errorHandler");
+
+const app = express();
+dotenv.config();
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => console.log("Database connection successful!"))
+  .catch((err) => console.log(err));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+app.use("/", loginRouter);
+// app.use("/users", usersRouter);
+// app.use("/inbox", inboxRouter);
+
+// 404 not found error handler
+app.use(notFoundHandler);
+
+// error handling
+app.use(errorHandler);
+
+// port number
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on ${process.env.PORT}`);
+});
